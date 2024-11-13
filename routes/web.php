c2a98fa382582c3fa26ed3f567018141812d35b3
minhajul <kmsohelinfo@gmail.com>
2024-11-13 17:05:52 +0600

    Added json response to home route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'success' => true,
+        'message' => 'Innoscripta News Aggregator APIs',
+        'version' => '1.0.0',
+        'ip' => request()->ip(),
+        'timestamp' => now()
+    ]);
 });
 
 
