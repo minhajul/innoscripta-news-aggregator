318e406be795473a68b3161444c7a6f8997b172f
minhajul <kmsohelinfo@gmail.com>
2024-11-16 03:39:19 +0600

    Updated

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command("app:fetch-nyt-articles climate change --limit=5")->daily();
-Artisan::command("app:fetch-bbc-articles technology --limit=5")->daily();
-Artisan::command("app:fetch-bbc-articles technology --limit=5")->daily();
+//Artisan::command("app:fetch-nyt-articles climate change --limit=5")->daily();
+//Artisan::command("app:fetch-bbc-articles technology --limit=5")->daily();
+//Artisan::command("app:fetch-bbc-articles technology --limit=5")->daily();
